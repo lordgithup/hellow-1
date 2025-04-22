fb9852267b37abb6d42df06258922f8b47333e8c
Kul@7489 <nagarkuldeep002@gmail.com>
2025-04-22 20:48:02 +0530

    intial-commit

---
+++ src/main/java/it/nxtvision/Hellow1Application.java
@@ -0,0 +1,13 @@
+package it.nxtvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hellow1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellow1Application.class, args);
+	}
+
+}
---
+++ src/main/java/it/nxtvision/conttroler/WelcomeController.java
@@ -0,0 +1,13 @@
+package it.nxtvision.conttroler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/h")
+    public String welcome() {
+        return "Welcome to NxtVision!";
+    }
+}
